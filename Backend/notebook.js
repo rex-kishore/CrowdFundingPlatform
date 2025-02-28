1baa5d9cadff80f2f5caac8c296cb1c10af46ed1
rex-kishore <kishorebook143@gmail.com>
2025-02-28 23:09:11 +0530

    Backend and Frontend in saperate folders updated

---
+++ Backend/notebook.js
@@ -0,0 +1,29 @@
+// controllers:
+// contains logic for handling HTTP request,create
+// campaigns or processing the donation
+
+// models:
+// contains Mongoose schema for data models
+
+// routes:
+// routes defination
+
+// .config:
+// includes the configuration files,eg:database connection
+// set up
+
+// Middleware:
+// handles the task authentication, validation error
+// handling
+// app.use(express.json());
+// client<------>middleware<------>server
+
+// Database Connection:
+// MongoDB connection using mongoose
+
+// dotenv:
+// .env file for storing environment variables
+
+
+// Step 1:
+// set up routes and controllers
